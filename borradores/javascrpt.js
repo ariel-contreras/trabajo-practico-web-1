933538d53dd25bfb81b3be5f4e07419b2e246ab1
GastonBerroa <165953469+GastonBerroa@users.noreply.github.com>
2025-05-07 16:13:50 -0300

    Add files via upload

---
+++ borradores/javascrpt.js
@@ -0,0 +1,8 @@
+const nav = document.querySelector ("nav ul");
+const abrir = document.querySelector (".abre");
+
+
+abrir.addEventListener ("click", () => {
+     nav.classList.toggle ("abrir")
+})
+
